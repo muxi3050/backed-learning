051d05b52767a3218cfe1d57ce652da692d4d687
fyk <305075210@qq.com>
2024-09-03 16:27:13 +0800

    feat:log

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 const { get, set } = require('./src/db/redis')
+const { access } = require('./src/utils/log')
 
 const getPostData = (req) => {
     const promise = new Promise((resolve, reject) => {
@@ -39,6 +40,9 @@ const getCookieExpires = () => {
 // const SESSION_DATA = {}
 
 const serverHandle = (req, res) => {
+    //记录access log
+    access(`${req.method} -- ${req.url} -- ${req.headers['user-agent']} -- ${Date.now()}`)
+
     //设置返回格式 JSON
     res.setHeader('Content-type', 'application/json')
     //获取path
